b4778ae2a158419f965ba9b9174d3f169d7c6f03
tkzc00 <2238983030@qq.com>
2024-02-15 10:05:28 +0800

    add: 前端项目用户登录功能完成

--- kongoj-frontend/src/App.vue
+++ kongoj-frontend/src/App.vue
@@ -11,24 +11,8 @@
 
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
 import { onMounted } from "vue";
 
-const router = useRouter();
-const store = useStore();
-router.beforeEach((to, from, next) => {
-  if (to.meta?.access === "admin") {
-    if (store.state.user?.loginUser?.userRole === "admin") {
-      next();
-    } else {
-      next("/noAuth");
-      return;
-    }
-  } else {
-    next();
-  }
-});
 /**
  * 全局初始化函数，有全局单次调用的代码，可以放在这里
  */
---
+++ kongoj-frontend/src/access/index.ts
@@ -0,0 +1,28 @@
+import router from "@/router";
+import store from "@/store";
+import ACCESS_ENUM from "@/access/accessEnum";
+import checkAccess from "@/access/checkAccess";
+
+router.beforeEach(async (to, from, next) => {
+  const loginUser = store.state.user.loginUser;
+  // 如果没有登录，自动登录
+  if (!loginUser || !loginUser.userRole)
+    // 加await是为了登录成功之后，再执行后续的代码
+    await store.dispatch("user/getLoginUser");
+  const needAccess = (to.meta?.access as string) ?? ACCESS_ENUM.NOT_LOGIN;
+  // 如果跳转的页面必须要登录
+  if (needAccess !== ACCESS_ENUM.NOT_LOGIN) {
+    // 如果未登录，跳转到登陆页面
+    if (!loginUser || !loginUser.userRole) {
+      next("/user/login?redirect=" + to.fullPath);
+      return;
+    }
+    // 检查权限
+    // 如果已经登陆了，但是权限不足，那么跳转到无权限页面
+    if (!checkAccess(loginUser, needAccess)) {
+      next("/noAuth");
+      return;
+    }
+  }
+  next();
+});
