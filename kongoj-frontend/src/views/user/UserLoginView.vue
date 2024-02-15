71ec4e68a3b44410ec0606e2eea33d7088d80255
tkzc00 <2238983030@qq.com>
2024-02-15 10:48:11 +0800

    add: 前端项目完成登录页面布局和登录逻辑

--- kongoj-frontend/src/views/user/UserLoginView.vue
+++ kongoj-frontend/src/views/user/UserLoginView.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+import { UserControllerService, UserLoginRequest } from "../../../generated";
+import message from "@arco-design/web-vue/es/message";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+/**
+ * 表单信息
+ */
+const form = reactive<UserLoginRequest>({
+  userAccount: "",
+  userPassword: "",
+});
+/**
+ * 提交表单
+ */
+const handleSubmit = async () => {
+  const res = await UserControllerService.userLoginUsingPost(form);
+  if (res.code === 0) {
+    // 修改全局登录状态，获取登录用户信息成功之后，才跳转到主页
+    await store.dispatch("user/getLoginUser");
+    // 登录成功，跳转到主页
+    router.push({
+      path: "/",
+      replace: true,
+    });
+    message.success("登录成功");
+  } else {
+    message.error("登录失败: ", res.message);
+  }
+};
+</script>
 
 <template>
-  <div id="userLogin">用户登录页面</div>
+  <div id="userLogin">
+    <a-form
+      :form="form"
+      style="width: 600px"
+      @submit="handleSubmit"
+      rules="required"
+    >
+      <a-form-item label="账号" tooltip="请输入账号" field="userAccount">
+        <a-input v-model="form.userAccount" placeholder="请输入账号" />
+      </a-form-item>
+      <a-form-item
+        label="密码"
+        tooltip="密码不少于8位"
+        field="userPassword"
+        rules="required"
+      >
+        <a-input-password
+          v-model="form.userPassword"
+          placeholder="请输入密码"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
 <style scoped></style>
