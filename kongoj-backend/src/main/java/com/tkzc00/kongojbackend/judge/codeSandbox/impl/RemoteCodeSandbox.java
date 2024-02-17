d5ed592965d81847a07fb42cc3c63cbf7ff72dc5
tkzc00 <2238983030@qq.com>
2024-02-17 17:01:25 +0800

    add: 完善调用的安全性

--- kongoj-backend/src/main/java/com/tkzc00/kongojbackend/judge/codeSandbox/impl/RemoteCodeSandbox.java
+++ kongoj-backend/src/main/java/com/tkzc00/kongojbackend/judge/codeSandbox/impl/RemoteCodeSandbox.java
@@ -13,11 +13,14 @@ import com.tkzc00.kongojbackend.judge.codeSandbox.model.ExecuteCodeResponse;
  * 远程代码沙箱（实际调用接口的沙箱）
  */
 public class RemoteCodeSandbox implements CodeSandbox {
+    public static final String AUTH_REQUEST_HEADER = "auth";
+    public static final String AUTH_REQUEST_SECRET = "secretKey";
+
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         String url = "http://localhost:8090/sandbox/executeCode";
         String json = JSONUtil.toJsonStr(executeCodeRequest);
-        String response = HttpUtil.createPost(url).body(json).execute().body();
+        String response = HttpUtil.createPost(url).body(json).header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET).execute().body();
         if (StrUtil.isBlank(response)) {
             throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "调用代码沙箱失败：" + response);
         }
--- kongoj-code-sandbox/src/main/java/com/tkzc00/kongojcodesandbox/controller/MainController.java
+++ kongoj-code-sandbox/src/main/java/com/tkzc00/kongojcodesandbox/controller/MainController.java
@@ -6,6 +6,8 @@ import com.tkzc00.kongojcodesandbox.model.ExecuteCodeResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/sandbox")
@@ -13,13 +15,23 @@ public class MainController {
     @Resource
     private JavaNativeCodeSandbox sandbox;
 
+    // 定义鉴权请求头和密钥
+    public static final String AUTH_REQUEST_HEADER = "auth";
+    public static final String AUTH_REQUEST_SECRET = "secretKey";
+
     @GetMapping("/health")
     public String healthCheck() {
         return "OK";
     }
 
     @PostMapping("/executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
+        // 进行基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
         if (executeCodeRequest == null)
             throw new RuntimeException("请求参数为空");
         return sandbox.executeCode(executeCodeRequest);
